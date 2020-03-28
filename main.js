19cb77756f98a626df517d041dacfd1e4c21b0ef
WangChong <wangchong@zhenrongbao.com>
2020-03-29 00:05:27 +0800

    第一次

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const __main = () => {
 
+const __main = () => {
+    let canvas = e('#canvas')
+    var context = canvas.getContext("2d");
+    let img = new Image()
+    img.src = 'img/paddle.png'
+    img.onload = function() {
+        context.drawImage(img, 50, 200)
+    }
 }
 __main()
---
+++ utils.js
@@ -0,0 +1,2 @@
+const e = (selector) => document.querySelector(selector)
+
